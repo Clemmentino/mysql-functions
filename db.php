e737dae49ae10a2d4543779c410436c1838665a5
Clemmentino <amoguis.clemm@dnscedu.onmicrosoft.com>
2026-02-06 18:39:32 +0800

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "mysql_all_functions";
+$dbname = "functions";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
